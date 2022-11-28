6aee435f00366bdb537619fd6e16b278b8b04482
Alpha Kaba <kabaa346@gmail.com>
2022-11-27 21:50:25 -0500

    welcome page implementation

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import screen_loader from './app/images/loader1.jpeg';
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <img src={screen_loader} alt="welcome_page" />
       <Text>WELCOME TO BEACON BUDDY</Text>
       <StatusBar style="auto" />
     </View>
